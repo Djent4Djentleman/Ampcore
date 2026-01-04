b49b1e7538112694055f26d42b0584f98d3db012
Djent4Djentleman <velmetalhead@gmail.com>
2026-01-04 16:58:22 -0600

    Initial Commit

---
+++ Ampcore/AmpcoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmpcoreApp.swift
+//  Ampcore
+//
+//  Created by Lubenets Valerii on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AmpcoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
